f858c524cbeb7040971d750a71a3900c62f3d781
Tomás Aguirre <taguirre@prospect-force.com>
2026-04-16 04:17:52 -0500

    refactor program

--- src/CornLimiter/CornLimiter/Program.cs
+++ src/CornLimiter/CornLimiter/Program.cs
@@ -22,127 +22,28 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-
-
-
-// FluentValidation
-builder.Services.AddFluentValidationAutoValidation();
-builder.Services.AddValidatorsFromAssemblyContaining<SellOneCommandValidator>();
-
-// Caching services
 builder.Services.AddMemoryCache();
+builder.Services.AddEndpointsApiExplorer();
 
-// Adds logging services and configures Exceptionless as a logging provider
-builder.Services.AddLogging(b => {
-    b.AddExceptionless();
-});
+ConfigureValidators(builder);
+ConfigureLogging(builder);
+ConfigureOptions(builder);
+ConfigureDbContext(builder);
+ConfigureRepositories(builder);
+ConfigureUseCases(builder);
+ConfigureSwagger(builder);
+ConfigureHealthChecks(builder);
+ConfigureMappingProfiles(builder);
+ConfigureVersioning(builder);
+ConfigureJwtAuthentication(builder);
 
-// Bind options
-builder.Services.Configure<CornLimiterOptions>(builder.Configuration.GetSection("CornLimiter"));
-builder.Services.Configure<ExceptionlessOptions>(builder.Configuration.GetSection("Exceptionless"));
-builder.Services.Configure<JwtTokenOptions>(builder.Configuration.GetSection("Jwt"));
-
-
-// Configure MySQL DbContext (usa ConnectionStrings:DefaultConnection)
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-if (!string.IsNullOrWhiteSpace(connectionString))
-{
-    builder.Services.AddDbContext<MySqlDbContext>(options =>
-        options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
-}
-
-
-// Registrar repositorios y casos de uso (inyección por interfaz)
-builder.Services.AddScoped<ISaleRepository, SaleRepository>();
-builder.Services.AddScoped<SellOneUseCase>();
-builder.Services.AddScoped<IUnitOfWork>(c => c.GetRequiredService<MySqlDbContext>());
-
-// Leer las opciones bindeadas para registrar Exceptionless (la sección ya está vinculada a IOptions)
 var exceptionlessSection = builder.Configuration.GetSection("Exceptionless");
 var exceptionlessOptions = exceptionlessSection.Get<ExceptionlessOptions>();
-if (!string.IsNullOrWhiteSpace(exceptionlessOptions?.ApiKey))
-{
-    builder.Services.AddExceptionless(exceptionlessOptions.ApiKey);
-    builder.Services.AddHttpContextAccessor();
-}
-
-// Swagger / OpenAPI
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Title = "CornLimiter API",
-        Version = "v1",
-        Description = "Documentación OpenAPI para CornLimiter"
-    });
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Name = "Authorization",
-        Type = SecuritySchemeType.Http,
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Description = "Enter 'Bearer' [space] and then your valid token."
-    });
-    c.AddSecurityRequirement(document => new OpenApiSecurityRequirement
-    {
-        [new OpenApiSecuritySchemeReference("Bearer", document)] = []
-    });
-});
-
-builder.Services
-    .AddHealthChecks()
-    .AddMySql(connectionString!);
-
-
-builder.Services.AddAutoMapper(cfg => { }, typeof(SaleMapProfile));
-
-builder.Services.AddApiVersioning(options =>
-{
-    options.DefaultApiVersion = new ApiVersion(1);
-    options.ReportApiVersions = true;
-    options.AssumeDefaultVersionWhenUnspecified = true;
-    options.ApiVersionReader = new UrlSegmentApiVersionReader();
-})
-.AddMvc() // This is needed for controllers
-.AddApiExplorer(options =>
-{
-    options.GroupNameFormat = "'v'V";
-    options.SubstituteApiVersionInUrl = true;
-});
-
-// Configurar autenticación JWT 
-var jwtSection = builder.Configuration.GetSection("Jwt");
-var jwtTokenOptions = jwtSection.Get<JwtTokenOptions>();
-
-if (jwtTokenOptions != null)
-{
-    builder.Services.AddAuthentication(options =>
-    {
-        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    }).AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = jwtTokenOptions.Issuer,
-            ValidAudience = jwtTokenOptions.Audience,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtTokenOptions.Key))
-        };
-    });
-}
-
+ConfigureExceptionsService(builder, exceptionlessOptions!);
 
 
 var app = builder.Build();
 
-
-// Habilitar Swagger y SwaggerUI en desarrollo
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -160,7 +61,6 @@ app.UseMiddleware<LoggerMiddleware>();
 app.UseMiddleware<MetricsMiddleware>();
 
 
-// Registrar middleware que reporta excepciones a Exceptionless (opcional si AddExceptionless fue registrado)
 if (!string.IsNullOrWhiteSpace(exceptionlessOptions?.ApiKey))
 {
     app.UseExceptionless();
@@ -180,3 +80,138 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+static void ConfigureValidators(WebApplicationBuilder builder)
+{
+     builder.Services.AddFluentValidationAutoValidation();
+    builder.Services.AddValidatorsFromAssemblyContaining<SellOneCommandValidator>();
+}
+
+static void ConfigureOptions(WebApplicationBuilder builder)
+{
+    builder.Services.Configure<CornLimiterOptions>(builder.Configuration.GetSection("CornLimiter"));
+    builder.Services.Configure<ExceptionlessOptions>(builder.Configuration.GetSection("Exceptionless"));
+    builder.Services.Configure<JwtTokenOptions>(builder.Configuration.GetSection("Jwt"));
+}
+
+static void ConfigureDbContext(WebApplicationBuilder builder)
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    if (!string.IsNullOrWhiteSpace(connectionString))
+    {
+        builder.Services.AddDbContext<MySqlDbContext>(options =>
+            options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+    }
+
+    builder.Services.AddScoped<IUnitOfWork>(c => c.GetRequiredService<MySqlDbContext>());
+}
+
+static void ConfigureExceptionsService(WebApplicationBuilder builder, ExceptionlessOptions exceptionlessOptions)
+{
+    if (!string.IsNullOrWhiteSpace(exceptionlessOptions?.ApiKey))
+    {
+        builder.Services.AddExceptionless(exceptionlessOptions.ApiKey);
+        builder.Services.AddHttpContextAccessor();
+    }
+}
+
+static void ConfigureSwagger(WebApplicationBuilder builder)
+{
+    builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Title = "CornLimiter API",
+            Version = "v1",
+            Description = "Documentación OpenAPI para CornLimiter"
+        });
+        c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            Name = "Authorization",
+            Type = SecuritySchemeType.Http,
+            Scheme = "Bearer",
+            BearerFormat = "JWT",
+            In = ParameterLocation.Header,
+            Description = "Enter 'Bearer' [space] and then your valid token."
+        });
+        c.AddSecurityRequirement(document => new OpenApiSecurityRequirement
+        {
+            [new OpenApiSecuritySchemeReference("Bearer", document)] = []
+        });
+    });
+}
+
+static void ConfigureHealthChecks(WebApplicationBuilder builder)
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    builder.Services
+        .AddHealthChecks()
+        .AddMySql(connectionString!);
+}
+
+static void ConfigureMappingProfiles(WebApplicationBuilder builder)
+{
+    builder.Services.AddAutoMapper(cfg => { }, typeof(SaleMapProfile));
+}
+
+static void ConfigureVersioning(WebApplicationBuilder builder)
+{
+    builder.Services.AddApiVersioning(options =>
+    {
+        options.DefaultApiVersion = new ApiVersion(1);
+        options.ReportApiVersions = true;
+        options.AssumeDefaultVersionWhenUnspecified = true;
+        options.ApiVersionReader = new UrlSegmentApiVersionReader();
+    })
+    .AddMvc() // This is needed for controllers
+    .AddApiExplorer(options =>
+    {
+        options.GroupNameFormat = "'v'V";
+        options.SubstituteApiVersionInUrl = true;
+    });
+}
+
+static void ConfigureJwtAuthentication(WebApplicationBuilder builder)
+{
+    var jwtSection = builder.Configuration.GetSection("Jwt");
+    var jwtTokenOptions = jwtSection.Get<JwtTokenOptions>();
+
+    if (jwtTokenOptions != null)
+    {
+        builder.Services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = jwtTokenOptions.Issuer,
+                ValidAudience = jwtTokenOptions.Audience,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtTokenOptions.Key))
+            };
+        });
+    }
+}
+
+static void ConfigureLogging(WebApplicationBuilder builder)
+{
+    builder.Services.AddLogging(b =>
+    {
+        b.AddExceptionless();
+    });
+}
+
+static void ConfigureRepositories(WebApplicationBuilder builder)
+{
+    builder.Services.AddScoped<ISaleRepository, SaleRepository>();
+}
+
+static void ConfigureUseCases(WebApplicationBuilder builder)
+{
+    builder.Services.AddScoped<SellOneUseCase>();
+}
