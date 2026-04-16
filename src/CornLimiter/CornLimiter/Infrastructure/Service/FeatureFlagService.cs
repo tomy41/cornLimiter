8b35c1b336ad1d6374cdb275499149748d3f7ece
Tomás Aguirre <taguirre@prospect-force.com>
2026-04-16 14:33:56 -0500

    move feature flag service

---
+++ src/CornLimiter/CornLimiter/Infrastructure/Service/FeatureFlagService.cs
@@ -0,0 +1,18 @@
+﻿using CornLimiter.Domain;
+using LaunchDarkly.Sdk.Server;
+
+namespace CornLimiter.Infrastructure.Service;
+
+public class FeatureFlagService(LdClient client) : IFeatureFlagsService
+{
+
+    public bool IsFeatureFlagEnabled(string flagKey)
+    {
+        var context = LaunchDarkly.Sdk.Context.Builder("anonymous")
+            .Name("anonymous")
+            .Build();
+        bool isFeatureEnabled = client.BoolVariation(flagKey, context, false);
+
+        return isFeatureEnabled;
+    }
+}
